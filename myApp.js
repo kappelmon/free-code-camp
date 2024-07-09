1415b312c3466c0f7c1bf3c96c85a82dc156d6da
kappelmon <kappelmon@gmail.com>
2024-07-08 18:52:52 -0700

    added nodejs path

--- myApp.js
+++ myApp.js
@@ -2,7 +2,6 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-const PORT = process.env.PORT || 3030;
 
 
 
@@ -61,6 +60,6 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log(`Useful Programmer Info Security App started on Port ${port}`);
 });
