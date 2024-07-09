4e7b0a0c3defb63f94831a507dadaf7a5056fa23
kappelmon <kappelmon@gmail.com>
2024-07-08 18:28:33 -0700

    Add initial application

--- myApp.js
+++ myApp.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const express = require('helmet');
+const helmet = require('helmet');
 const app = express();
 
 const PORT = process.env.PORT || 3030;
 
-app.listen(PORT, () => {
-  console.log('Useful Programmer Info Security App started on port ${PORT}');
-});
+
 
 
 
@@ -63,6 +61,6 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log('Useful Programmer Info Security App started on port ${PORT}');
 });
